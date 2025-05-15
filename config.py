7049ec6353efc9666e29666e11016c1e6337706b
andysx420 <147619538+andysx420@users.noreply.github.com>
2025-05-15 11:25:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
-auth_users = [5840594311]
+api_id = os.getenv("API_ID")
+api_hash = os.getenv("API_HASH")
+bot_token = os.getenv("BOT_TOKEN")
+auth_users = list(map(int, os.getenv("AUTH_USERS").split(',')))
