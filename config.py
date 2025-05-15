f5715270916a0488cbc7b1d33af94228711c472f
andysx420 <147619538+andysx420@users.noreply.github.com>
2025-05-15 12:00:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "24473318"
-api_hash = "e7dd0576c5ac0ff8f90971d6bb04c8f5"
-bot_token = "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0"
+api_id = "28412093"
+api_hash = "254e985f3e835dc0b89607fe0dbfa9ae"
+bot_token = "7953653056:AAGNyBPtlpBrnR-3kNrnmA0g3z7dtSk7Ux0"
 auth_users = [1548230757]
