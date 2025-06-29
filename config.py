e7b9e1534d167138d53cefd4256d610e8cbd440c
andysx420 <147619538+andysx420@users.noreply.github.com>
2025-06-29 11:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "28412093"
 api_hash = "254e985f3e835dc0b89607fe0dbfa9ae"
 bot_token = "7953653056:AAGNyBPtlpBrnR-3kNrnmA0g3z7dtSk7Ux0"
-auth_users = [1548230757]
+auth_users = [1615865254]
